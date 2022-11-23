49ef1070ac3d9b76394ac52c7120baf1bda1112c
ABDUR RAHMAN <abdurrahman100800@gmail.com>
2022-11-23 23:35:09 +0530

    updated some changes

--- abdur_Java/src/abdur_Java/DbExampleGetInfo.java
+++ abdur_Java/src/abdur_Java/DbExampleGetInfo.java
@@ -7,12 +7,21 @@ public class DbExampleGetInfo {
 				+ ":5432/abdur", "postgres", "1234");
 		PreparedStatement ps = con.prepareStatement("SELECT * FROM personal");
 		ps.executeQuery();
-		/* Used to get the meta data of the database */
+		/* Used to get the meta data of the database tables */
+		/* we should use PreparedStatement obj because we're getting the info
+		 * of database tables */
 		ResultSetMetaData rsmd = ps.getMetaData();
 		System.out.println("1st column name : " + rsmd.getColumnName(1));
 		System.out.println("2nd column name : " + rsmd.getColumnName(2));
 		System.out.println("3rd column name : " + rsmd.getColumnName(3));
 		System.out.println("4th column name : " + rsmd.getColumnName(4));
 		System.out.println("Total column count : " + rsmd.getColumnCount());
+		
+		/* Used to get the meta data of the database itself like the version
+		 * number, DB name, etc... */
+		DatabaseMetaData dmd = con.getMetaData();
+		System.out.println("Database name : " + dmd.getDatabaseProductName());
+		System.out.println("Database version : " + dmd.getDatabaseProductVersion());
+		
 	}
 }
