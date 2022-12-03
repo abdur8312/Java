133d2cff86eb3f39dca59c629206908ddc4ba09b
ABDUR RAHMAN <abdurrahman100800@gmail.com>
2022-12-03 17:40:11 +0530

    going to reset

--- abdur_Java/src/abdur_Java/PatternPrinting.java
+++ abdur_Java/src/abdur_Java/PatternPrinting.java
@@ -5,11 +5,11 @@ import java.util.Scanner;
 public class PatternPrinting {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int r = 5; //sc.nextInt(); 
-		int c = 6; //sc.nextInt();
-		for(int i = 1; i <= r; i++) {
-			for(int j = 1; j <= c; j++) {
-				if(i == 1 || i == r || j == 1 || j == c) {
+		int row = 5; //sc.nextInt(); 
+		int col = 6; //sc.nextInt();
+		for(int i = 1; i <= row; i++) {
+			for(int j = 1; j <= col; j++) {
+				if(i == 1 || i == row || j == 1 || j == col) {
 					System.out.print("*");
 				}else {
 					System.out.print(" ");
