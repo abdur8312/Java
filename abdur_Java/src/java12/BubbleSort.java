182b8d603c7e94d8e83073352f0da5b21519ce92
ABDUR RAHMAN <abdurrahman100800@gmail.com>
2022-12-06 19:28:18 +0530

    added method for descending order

--- abdur_Java/src/java12/BubbleSort.java
+++ abdur_Java/src/java12/BubbleSort.java
@@ -18,12 +18,44 @@ public class BubbleSort {
 		return a;
 	}
 	
+	public List<Integer> descendingOrderList(List<Integer> a){
+		int n = a.size();
+		for(int i = 0; i < a.size()-1; i++) {
+			int temp = 0;
+			for(int j = 1; j < n; j++) {
+				if(a.get(temp) < a.get(j)) {
+					Collections.swap(a, temp, j);
+				}
+				temp += 1;
+			}
+			n -= 1;
+		}
+		return a;
+	}
+	
 	public int[] ascendingOrderArray(int[] a){
 		int n = a.length;
 		for(int i = 0; i < a.length-1; i++) {
 			int temp = 0;
 			for(int j = 1; j < n; j++) {
 				if(a[temp] > a[j]) {
+					int c = a[temp];
+					a[temp] = a[j];
+					a[j] = c;
+				}
+				temp += 1;
+			}
+			n -= 1;
+		}
+		return a;
+	}
+	
+	public int[] descendingOrderArray(int[] a){
+		int n = a.length;
+		for(int i = 0; i < a.length-1; i++) {
+			int temp = 0;
+			for(int j = 1; j < n; j++) {
+				if(a[temp] < a[j]) {
 					int c = a[temp];
 					a[temp] = a[j];
 					a[j] = c;
